bf0861148ff618bbe04e0f2c19764267a466a146
Malcolmt129 <malcolm.t129@gmail.com>
2023-04-18 12:36:07 -0400

    Finally got PIT working. Problem 3 is complete

---
+++ exam1/src/problem3/Fizzbuzz3Test.java
@@ -0,0 +1,61 @@
+package problem3;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Fizzbuzz3Test {
+	
+	Fizzbuzz3 fb;
+	@Before
+	public void setUp() throws Exception {
+		fb = new Fizzbuzz3();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		fb = null;
+	}
+	@Test
+	public void testFizzBuzzMethodValid() {
+		String output = fb.fizzBuzzMethod(15);
+		assertEquals(" fizz buzz fizz buzz fizz", output);
+	}
+   
+	@Test
+	public void testFizzBuzzMethodLessThani() {
+		String output = fb.fizzBuzzMethod(0);
+		assertEquals("", output);
+	}
+   
+	@Test
+	public void testFizzBuzzMethodForNumEqualsOne() {
+      
+		String output = fb.fizzBuzzMethod(1);
+		assertEquals("", output);
+	}
+   
+	@Test
+	public void testFizzBuzzMethodForNumDivisibleBy5() {
+      
+		String output = fb.fizzBuzzMethod(5);
+		assertEquals(" fizz", output);
+	}
+   
+	@Test
+	public void testFizzBuzzMethodForNumDivisibleBy7() {
+      
+		String output = fb.fizzBuzzMethod(7);
+		assertEquals(" fizz buzz", output);
+	}
+   
+	@Test
+	public void testFizzBuzzMethodForNumDivisibleBy5And7() {
+      
+		String output = fb.fizzBuzzMethod(35);
+		assertEquals(" fizz buzz fizz buzz fizz fizz buzz fizz buzz fizz fizzbuzz", output);
+	}
+
+}
